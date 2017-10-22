e2d83c85db9b5321e92e945395d8cae528af9843
user <siwar.madrane@gmail.com>
2017-10-22 13:55:44 +0200

    Commit 2 - Package DAO

---
+++ BanqueSI/src/main/java/org/glsid/dao/OperationRepository.java
@@ -0,0 +1,8 @@
+package org.glsid.dao;
+
+import org.glsid.entities.Operation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OperationRepository extends JpaRepository<Operation, Long> {
+
+}
